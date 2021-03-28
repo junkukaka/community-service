afa033b8c399b3e930a496bb60be1070a14e6b27
junkukaka <342421055@qq.com>
2021-03-28 20:25:35 +0800

    user login

--- src/test/java/com/community/aspn/AspnApplicationTests.java
+++ src/test/java/com/community/aspn/AspnApplicationTests.java
@@ -18,62 +18,62 @@ import java.util.regex.Pattern;
 @SpringBootTest
 class AspnApplicationTests {
 
-    @Test
-    void minoUtilTest(){
-        String communityFileName = MinIOFileUtil.getCommunityFileName("abc.jpg");
-        System.out.println(communityFileName);
-        System.out.println(MinIOFileUtil.getCommunityFileName());
-    }
-
-    @Test
-    void testRes(){
-        String content = "<img src=\"data:image/jepg;base64,iVBORANX4xQVR4Ae3d2Y8UZd/Gcc6e/+AORK5CYII=\">" ;
-        String pattern = "(data:image\\/[a-z]{2,4}\\;base64\\,)[a-z,A-Z]([^\'\"]+)";
-
-        Pattern r = Pattern.compile(pattern);
-        Matcher matcher = r.matcher(content);
-        String replace = content;
-
-        while (matcher.find()){
-            String group = matcher.group();
-            replace = replace.replace(group, "123");
-            System.out.println(replace);
-        }
-    }
-
-    @Test
-    void testBaseToFile(){
-        String content = "<img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAacAAAEnCAYAAAANX4xAAAAf6ElEQVR4Ae3d2Y8UZd/Gcc6e/+AORK5CYII=\">" ;
-        Boolean aBoolean = MinIOFileUtil.ifBase64RegexMatcher(content);
-        System.out.println(aBoolean);
-    }
-
-    @Test
-    void testTokenToString(){
-        Member member = new Member();
-        member.setId(1);
-        String token = TokenUtil.sign(member);
-        System.out.println("token = " + token);
-
-        JWTVerifier verifier = JWT.require(Algorithm.HMAC256("txdy")).build();
-        DecodedJWT jwt = verifier.verify(token);
-        int t = jwt.getClaim("userId").asInt();
-        System.out.println(t);
-    }
-
-    @Test
-    void testEmailRegex(){
-        String regex = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
-        String[] str1 = {"lu.jiayu@ncbakery.com",
-                "cs1@chefchoice-china.com",
-                "cs2@dd.com",
-                "aa.b@qq.com","1123@163.com","113fe@11.com",
-                "han. @sohu.com.cn","han.c@sohu.com.cn.cm.cm",
-                "","张吗"};
-        for (String str:str1){
-            System.out.println(str+" \\\\. "+str.matches(regex));
-        }
-    }
+//    @Test
+//    void minoUtilTest(){
+//        String communityFileName = MinIOFileUtil.getCommunityFileName("abc.jpg");
+//        System.out.println(communityFileName);
+//        System.out.println(MinIOFileUtil.getCommunityFileName());
+//    }
+//
+//    @Test
+//    void testRes(){
+//        String content = "<img src=\"data:image/jepg;base64,iVBORANX4xQVR4Ae3d2Y8UZd/Gcc6e/+AORK5CYII=\">" ;
+//        String pattern = "(data:image\\/[a-z]{2,4}\\;base64\\,)[a-z,A-Z]([^\'\"]+)";
+//
+//        Pattern r = Pattern.compile(pattern);
+//        Matcher matcher = r.matcher(content);
+//        String replace = content;
+//
+//        while (matcher.find()){
+//            String group = matcher.group();
+//            replace = replace.replace(group, "123");
+//            System.out.println(replace);
+//        }
+//    }
+//
+//    @Test
+//    void testBaseToFile(){
+//        String content = "<img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAacAAAEnCAYAAAANX4xAAAAf6ElEQVR4Ae3d2Y8UZd/Gcc6e/+AORK5CYII=\">" ;
+//        Boolean aBoolean = MinIOFileUtil.ifBase64RegexMatcher(content);
+//        System.out.println(aBoolean);
+//    }
+//
+//    @Test
+//    void testTokenToString(){
+//        Member member = new Member();
+//        member.setId(1);
+//        String token = TokenUtil.sign(member);
+//        System.out.println("token = " + token);
+//
+//        JWTVerifier verifier = JWT.require(Algorithm.HMAC256("txdy")).build();
+//        DecodedJWT jwt = verifier.verify(token);
+//        int t = jwt.getClaim("userId").asInt();
+//        System.out.println(t);
+//    }
+//
+//    @Test
+//    void testEmailRegex(){
+//        String regex = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+//        String[] str1 = {"lu.jiayu@ncbakery.com",
+//                "cs1@chefchoice-china.com",
+//                "cs2@dd.com",
+//                "aa.b@qq.com","1123@163.com","113fe@11.com",
+//                "han. @sohu.com.cn","han.c@sohu.com.cn.cm.cm",
+//                "","张吗"};
+//        for (String str:str1){
+//            System.out.println(str+" \\\\. "+str.matches(regex));
+//        }
+//    }
 
 
 }
